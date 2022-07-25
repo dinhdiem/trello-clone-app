bf104025bea71867e8990bc9c04e4cd774087b19
dinhdiem <miktendinh@gmail.com>
2022-07-26 00:05:35 +0700

    update data card, column

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,16 +1,31 @@
-import Column from "components/Column/Column";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { isEmpty } from "lodash";
 import "./BoardContent.scss";
+import Column from "components/Column/Column";
+import { data } from "actions/initialData";
+import { mapOrder } from "utilities/sorts";
 
 const BoardContent = () => {
+  const [board, setBoard] = useState({});
+  const [columns, setColumns] = useState({});
+
+  useEffect(() => {
+    const boardFormDB = data.boards.find((board) => board.id === "board-1");
+    if (boardFormDB) {
+      setBoard(boardFormDB);
+      setColumns(mapOrder(boardFormDB.columns, boardFormDB.columsOrder, "id"));
+    }
+  }, []);
+
+  if (isEmpty(board)) {
+    return <h1>Not Found</h1>;
+  }
+
   return (
     <div className="board-content">
-      <Column />
-      <Column />
-      <Column />
-      <Column />
-      <Column />
-      <Column />
+      {columns.map((column, index) => (
+        <Column key={index} column={column} />
+      ))}
     </div>
   );
 };
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,24 +1,19 @@
-import Task from "components/Task/Task";
+import Card from "components/Card/Card";
 import React from "react";
+import { mapOrder } from "utilities/sorts";
 
 import "./Column.scss";
 
-const Column = () => {
+const Column = (props) => {
+  const { column } = props;
+  const cards = mapOrder(column.cards, column.cardOrder, "id");
   return (
     <div className="column">
-      <header>Board Bar</header>
-      <ul className="task-list">
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        {/* <li>Add what do you thinks you thinks you thinks you thinks</li>
-        <li>Add what do you thinks</li>
-        <li>Add what do you thinks</li>
-        <li>Add what do you thinks</li>
-        <li>Add what do you thinks</li>
-        <li>Add what do you thinks</li>
-        <li>Add what do you thinks</li> */}
+      <header>{column.title}</header>
+      <ul className="card-list">
+        {cards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
       </ul>
       <footer>Add another card</footer>
     </div>
